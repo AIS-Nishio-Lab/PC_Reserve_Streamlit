898120222f9cd3afd6eee834224bbe5224444fad
AIS-Nishio-Lab <115961846+AIS-Nishio-Lab@users.noreply.github.com>
2024-01-19 23:57:50 +0900

    Update 1_Cancel_Reservation.py

--- pages/1_Cancel_Reservation.py
+++ pages/1_Cancel_Reservation.py
@@ -97,8 +97,7 @@ if __name__ == "__main__":
     img_icon = Image.open("icon.jpg")
     st.set_page_config(
         page_title="PC Reserve App",
-        page_icon=img_icon,
-        layout="wide"
+        page_icon=img_icon
     )
     st.title("PC Reservation System")
     is_logined, username = login()
