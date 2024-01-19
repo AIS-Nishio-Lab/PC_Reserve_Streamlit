fc28ebc614d486c56a1a3a49b1ef299b36bb8e03
AIS-Nishio-Lab <115961846+AIS-Nishio-Lab@users.noreply.github.com>
2024-01-19 23:57:28 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -185,8 +185,7 @@ if __name__ == '__main__':
     img_icon = Image.open("icon.jpg")
     st.set_page_config(
         page_title="PC Reserve App",
-        page_icon=img_icon,
-        layout="wide"
+        page_icon=img_icon
     )
     st.title("PC Reserve System") # タイトル
     is_logined, username = login()
