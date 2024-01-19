955013afd78ff994f033959e36640b34b4923040
AIS-Nishio-Lab <115961846+AIS-Nishio-Lab@users.noreply.github.com>
2024-01-19 23:54:54 +0900

    Update 4_Logout.py

--- pages/4_Logout.py
+++ pages/4_Logout.py
@@ -2,8 +2,15 @@ import yaml
 from yaml.loader import SafeLoader
 import streamlit as st
 import streamlit_authenticator as stauth
+from PIL import Image
 
 if __name__ == "__main__":
+    img_icon = Image.open("icon.jpg")
+    st.set_page_config(
+        page_title="PC Reserve App",
+        page_icon=img_icon,
+        layout="wide"
+    )
     
     with open('config.yml') as file:
         config = yaml.load(file, Loader=SafeLoader)
