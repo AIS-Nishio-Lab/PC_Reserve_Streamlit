81b552304195d9f11e3b1cfcd52db0fd416ab80c
AIS-Nishio-Lab <115961846+AIS-Nishio-Lab@users.noreply.github.com>
2024-03-13 00:09:20 +0900

    Create spred_sheet_manager.py

---
+++ spred_sheet_manager.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+
+def read_spreadsheet(is_use_cache=True):
+    if not is_use_cache:
+        st.cache_data.clear()
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    df = conn.read()
+    df = df.dropna(how="all").dropna(how='all', axis=1)
+    # column1 to int
+    df.iloc[:,0] = df.iloc[:,0].astype(int)
+    # start column to timestamp
+    df.iloc[:,3] = pd.to_datetime(df.iloc[:,3])
+    df.iloc[:,4] = pd.to_datetime(df.iloc[:,4])
+    df.to_csv("pc_reserves.csv", index=False)
+
+def update_spreadsheet():
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    df = pd.read_csv("pc_reserves.csv")
+    conn.update(data=df)
+    st.cache_data.clear()
