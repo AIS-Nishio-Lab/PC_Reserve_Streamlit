c5ebc68b42c30ae00e3bd405011aeb852e1f1176
AIS-Nishio-Lab <115961846+AIS-Nishio-Lab@users.noreply.github.com>
2024-03-12 14:07:59 +0900

    Create git_file.py

---
+++ git_file.py
@@ -0,0 +1,26 @@
+from github import Github
+
+def get_pc_reserve_csv_from_github():
+    # リポジトリのURL
+    repo_name = "AIS-Nishio-Lab/PC_Reserve_Streamlit"
+    file_name = "pc_reserves.csv"
+    g = Github()
+    repo = g.get_repo(repo_name)
+    contents = repo.get_contents(file_name)
+    content = contents.decoded_content.decode("utf-8")
+    # ファイルを保存
+    with open(file_name, "w") as f:
+        f.write(content)
+
+def write_pc_reserve_csv_to_github():
+    # リポジトリのURL
+    repo_name = "AIS-Nishio-Lab/PC_Reserve_Streamlit"
+    file_name = "pc_reserves.csv"
+    token = "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"
+    g = Github(token)
+    repo = g.get_repo(repo_name)
+    # csvファイルをエンコード
+    with open("pc_reserves.csv", "r") as file:
+        csv = file.read()
+    update_file_class = repo.get_contents(file_name)
+    repo.update_file(file_name, "update pc_reserves.csv", csv, update_file_class.sha)
